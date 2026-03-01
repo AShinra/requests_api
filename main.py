7b7a8bbb6dc050f4be255adf7c3a5c0bf65b21af
Athan <jonpuray@gmail.com>
2026-03-01 16:25:29 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
+from pydantic import BaseModel
+from dotenv import load_dotenv
+from bson import ObjectId
+
+load_dotenv()
+
+app = FastAPI()
+
+
+# MongoDB connection
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["requestdb"]
+collection = db["users"]
+
+class User(BaseModel):
+    name: str
+    username: str
+
+@app.get("/")
+def home():
+    return {"status": "API is running"}
+
+@app.post("/users")
+def create_user(user: User):
+    result = collection.insert_one(user.model_dump())
+    return {
+        "message": "User created",
+        "id": str(result.inserted_id)
+    }
+
+@app.get("/users")
+def get_users():
+    users = []
+    for user in collection.find():
+        user["_id"] = str(user["_id"])
+        user['leave_credits'] = str(user["leave_credits"])
+        users.append(user)
+    return users
+
+@app.get("/users/{name}")
+def get_user(name: str):
+    user = collection.find_one({"name": name})
+
+    if not user:
+        raise HTTPException(status_code=404, detail="No such user")
+
+    user["_id"] = str(user["_id"])
+    return user
