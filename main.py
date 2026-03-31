569ffc4e4b49e277c108c307f80da5e0f8bfc7e5
Athan <jonpuray@gmail.com>
2026-03-31 10:14:53 +0800

    updated

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def get_users():
     users = []
     for user in collection.find():
         user["_id"] = str(user["_id"])
-        user['leave_credits'] = str(user["leave_credits"])
+        user['name'] = str(user["name"])
         users.append(user)
     return users
 
